e1ae4f9e11a65a70b6c43c9aa148917823005522
longvl1234 <longvl0208@gmail.com>
2018-10-27 18:20:12 +0000

    first commit

---
+++ routes/todos.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+    router =express.Router(),
+    db = require("../models"),
+    helpers =require("../helpers/todos");
+    
+    router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo)
+    
+    router.route('/:todoId')
+    .get(helpers.showTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
+    
+    
+    
+    module.exports = router;
